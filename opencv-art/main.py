1ed02bd65467ae9c50bdfeb2a27af4a1c61cecd1
Satyen Subramaniam <66653384+SubramaniamSatyen@users.noreply.github.com>
2024-04-26 14:53:01 -0700

    Adding transformation specific args

--- opencv-art/main.py
+++ opencv-art/main.py
@@ -12,24 +12,29 @@ for op in valid_ops:
 parser.add_argument("--all", dest="ops", action="store_const", const=valid_ops)
 parser.add_argument("--display", dest="display", action="store_true")
 
+parser.add_argument("-s", "--sigma-s", default=60, dest="sigma_s")
+parser.add_argument("-r", "--sigma-r", default=0.6, dest="sigma_r")
+parser.add_argument("-f", "--shade-factor", default=0.05, dest="shade_factor")
+parser.add_argument("-b", "--brush-size", default=7, dest="brush_size")
+
 args = parser.parse_args()
 
 img = cv2.imread(args.img_path)    
 for op in args.ops or [valid_ops[0]]:
     if (op == "oil-painting"):
-        res = cv2.xphoto.oilPainting(img, 7, 1)
+        res = cv2.xphoto.oilPainting(img, args.brush_size, 1)
 
     elif (op == "watercolor"):
-        res = cv2.stylization(img, sigma_s=60, sigma_r=0.6)
+        res = cv2.stylization(img, sigma_s=args.sigma_s, sigma_r=args.sigma_r)
 
     elif (op == "cartoon"):
-        res = cv2.medianBlur(img, 7)
+        res = cv2.medianBlur(img, args.brush_size)
 
     elif (op == "sketch-bw"):
-        res, _ = cv2.pencilSketch(img, sigma_s=60, sigma_r=0.07, shade_factor=0.05) 
+        res, _ = cv2.pencilSketch(img, sigma_s=args.sigma_s, sigma_r=args.sigma_r, shade_factor=args.shade_factor) 
         
     elif (op == "sketch-c"):
-        _, res = cv2.pencilSketch(img, sigma_s=60, sigma_r=0.07, shade_factor=0.05) 
+        _, res = cv2.pencilSketch(img, sigma_s=args.sigma_s, sigma_r=args.sigma_r, shade_factor=args.shade_factor) 
 
     elif (op == "pop"):
         res = pop.popify_image(img, background_color=[255, 255, 255], dot_color=(0, 0, 0))
